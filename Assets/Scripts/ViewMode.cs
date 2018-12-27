ac6c9eb7d1c61ff9d1f53b489e90bc8aa3caa3e4
Bottemanne Fany <fany.bottemanne@etudiant.univ-lille1.fr>
2018-12-27 14:26:50 +0100

    add first implementation view mode

---
+++ Assets/Scripts/ViewMode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewMode : MonoBehaviour {
+
+    private bool freeView = true;
+
+    //TODO est ce util? ca cause des complications: on ne sait plus tourner la tete dans la video + ajout de parent
+    //private bool followView = false; 
+
+    [Tooltip("The video that the people can not leave")]
+    private GameObject objectToFixed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+        /*if(objectToFixed != null && !followView) {
+            objectToFixed.transform.parent = null;
+        }*/
+        if (freeView) {
+            GetComponent<FirstPersonControls>().fixedView = false; //TODO en fonction de si on est avec la vr ou pas
+        } else {
+            /*if (followView) {
+                GetComponent<FirstPersonControls>().fixedView = false;
+                objectToFixed.transform.position = transform.position;
+                objectToFixed.transform.parent = transform;
+            } else {*/
+                GetComponent<FirstPersonControls>().fixedView = true;
+                transform.position = objectToFixed.transform.position;
+            //}
+        }
+	}
+
+    public void changeView(bool freeView, GameObject objectToFixed) {
+        this.freeView = freeView;
+        //this.followView = followView;
+        this.objectToFixed = objectToFixed;
+    }
+}
