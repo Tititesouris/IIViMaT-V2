fa0d1a95f99b7d469f9ebcaeb69bb811a08c0fbc
Tititesouris <tititesouris@laposte.net>
2019-01-10 14:12:32 +0100

    Visual reactions to apply effects on camera

---
+++ Assets/Scripts/Interaction/Reactions/VisualReactions/FadeOutVisualReaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Interaction.Reactions.VisualReactions
+{
+    public class FadeOutVisualReaction : VisualReaction
+    {
+        private Vignette _vignette;
+
+        protected override void StartEffect()
+        {
+            _vignette = Effects.OfType<Vignette>().First();
+            _vignette.enabled.Override(true);
+            _vignette.intensity.Override(50f);
+        }
+
+        protected override void ApplyEffect(float value)
+        {
+            _vignette.intensity.value = Mathf.Exp(1 + 2 * value) - Mathf.Exp(1);
+        }
+
+        protected override void ResetEffect()
+        {
+            _vignette.enabled.Override(false);
+        }
+    }
+}
