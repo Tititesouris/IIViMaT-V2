2898e9b81ab8656dad9d9efa3a0c80fa275fa699
Brault Quentin <brault@b02p16.FIL>
2019-01-30 14:29:26 +0100

    add editor for videoTimeAction

---
+++ Assets/Editor/VideoTimeActionEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Interaction.Actions;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VideoTimeAction), true)]
+public class VideoTimeActionEditor : ActionEditor
+{
+    private SerializedProperty _repeat;
+
+    private SerializedProperty _interval;
+
+    protected override void LoadGui()
+    {
+        base.LoadGui();
+        _repeat = serializedObject.FindProperty("repeat");
+        _interval = serializedObject.FindProperty("interval");
+    }
+
+    protected override void DrawGui()
+    {
+        base.DrawGui();
+        var action = (VideoTimeAction)target;
+        EditorGUILayout.PropertyField(_repeat);
+        if (action.repeat)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_interval);
+            EditorGUI.indentLevel--;
+        }
+    }
+
+    protected override IEnumerable<string> GetIgnoredFields()
+    {
+        var ignoredFields = new List<string> { "interval", "repeat" };
+        ignoredFields.AddRange(base.GetIgnoredFields());
+        return ignoredFields;
+    }
+}
